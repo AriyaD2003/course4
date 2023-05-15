77c3f79e3f7cf97fd71265069f8f40036d8f0b41
AriyaD2003 <132356470+AriyaD2003@users.noreply.github.com>
2023-05-16 01:20:14 +0500

    Add files via upload

---
+++ course4/project/services/users_service.py
@@ -0,0 +1,46 @@
+from project.dao import UserDAO
+from project.exceptions import ItemNotFound
+from project.schemas.user import UserSchema
+from project.services.base import BaseService
+from project.tools.security import generate_password_digest
+from flask_login import current_user
+
+class UsersService(BaseService):
+    def get_item_by_id(self, pk):
+        user = UserDAO(self._db_session).get_by_id(pk)
+        if not user:
+            raise ItemNotFound
+        return UserSchema().dump(user)
+
+    def get_item_by_email(self, email):
+        user = UserDAO(self._db_session).get_by_email(email)
+        if not user:
+            raise ItemNotFound
+        return UserSchema().dump(user)
+
+    def get_current_user(self):
+        user_id = current_user.get_id()
+        if user_id:
+            return self.get_item_by_id(user_id)
+        else:
+            raise ItemNotFound
+
+    def get_all_users(self):
+        users = UserDAO(self._db_session).get_all()
+        return UserSchema(many=True).dump(users)
+
+    def create(self, new_pd):
+        user_password = new_pd.get("password")
+        if user_password:
+            new_pd["password"] = generate_password_digest(user_password)
+        user = UserDAO(self._db_session).create(new_pd)
+        return UserSchema(many=True).dump(user)
+
+    def update(self, new_pd):
+        user = UserDAO(self._db_session).update(new_pd)
+        return UserSchema().dump(user)
+
+
+    def update_password(self, new_pd):
+        new_pd.get("password_1")
+        new_pd.get("password_2")
---
+++ course4/project/views/users.py
@@ -0,0 +1,65 @@
+from flask import request
+from flask_login import current_user, login_required
+from flask_restx import abort, Namespace, Resource
+from project.exceptions import ItemNotFound
+from project.services import UsersService
+from project.setup_db import db
+
+
+users_ns = Namespace("user")
+
+@users_ns.route("/")
+class UserView(Resource):
+    @login_required
+    @users_ns.response(200, "OK")
+    @users_ns.response(404, "User не найден")
+    def get(self):
+        """Get current user by token"""
+        user = UsersService(db.session).get_current_user()
+        if user:
+            return user
+        else:
+            abort(404, message="User `не найден`")
+
+
+
+@users_ns.route("/<int:user_id>")
+class UserByIdView(Resource):
+    @users_ns.response(200, "OK")
+    @users_ns.response(404, "User не найден")
+    def get(self, user_id: int):
+        """Get user by id"""
+        try:
+            return UsersService(db.session).get_item_by_id(user_id)
+        except ItemNotFound:
+            abort(404, message="User `не найден`")
+
+    def patch(self, user_id: int):
+        req_json = request.json
+        if not req_json:
+            abort(400, "Повторите запрос")
+        if not req_json.get("id"):
+            req_json['id'] = user_id
+        try:
+            return UsersService(db.session).update(req_json)
+        except ItemNotFound:
+            abort(404, message="User `не найден`")
+
+@users_ns.route("/password/<int:user_id>")
+class UserPatchView(Resource):
+    @users_ns.response(200, "OK")
+    @users_ns.response(404, "User не найден")
+    def put(self, user_id:int):
+        req_json = request.json
+        password_1 = req_json.get("password_1", None)
+        password_2 = req_json.get("password_2", None)
+        if None in [password_1, password_2]:
+            abort(400, "Повторите запрос")
+        if not password_1 or not password_2:
+            abort(400, "Повторите запрос")
+        if not req_json.get("id"):
+            req_json['id'] = user_id
+        try:
+            return UsersService(db.session).update_password(req_json)
+        except ItemNotFound:
+            abort(404, message="User `не найден`")
