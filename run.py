cbd6bf8dfc05f5095d9ae83e9ce0e6dcb7bb5a9b
AriyaD2003 <132356470+AriyaD2003@users.noreply.github.com>
2023-05-16 00:25:26 +0500

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
-from project.config import config
-from project.models import Genre
-from project.server import create_app, db
+from project.config import DevelopmentConfig
+from project.dao.models import Genre, User, Director, Movie
+from project.server.server import create_app, db
 
-app = create_app(config)
+app = create_app(DevelopmentConfig)
 
 
 @app.shell_context_processor
@@ -10,4 +10,11 @@ def shell():
     return {
         "db": db,
         "Genre": Genre,
+        "User": User,
+        "Director": Director,
+        "Movie": Movie,
     }
+
+
+if __name__ == '__main__':
+    app.run(port=8000)
