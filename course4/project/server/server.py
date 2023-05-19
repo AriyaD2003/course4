378883c23a6b10bf86a76671d6c38453f87867e6
AriyaD2003 <132356470+AriyaD2003@users.noreply.github.com>
2023-05-19 19:18:47 +0500

    Update server.py

--- course4/project/server/server.py
+++ course4/project/server/server.py
@@ -29,3 +29,51 @@ def create_app():
     api.add_namespace(auth_ns)
 
     return app
+
+
+#
+# from flask import Flask, Config, render_template
+# from flask_cors import CORS
+# from flask_restx import Api
+# from project.setup_db import db
+# from project.views import genres_ns
+# from project.views import directors_ns
+# from project.views import users_ns
+# from project.views import movies_ns
+# from project.views import auth_ns
+#
+# api = Api(
+#     authorizations={
+#         "Bearer": {"type": "apiKey", "in": "header", "name": "Authorization"}
+#     },
+#     title="Flask Course Project 3",
+#     doc="/docs",
+# )
+#
+# # Фронтенд
+# cors = CORS()
+#
+#
+# def create_app(config_obj):
+#     app = Flask(__name__)
+#     app.config.from_object(config_obj)
+#     register_extensions(app)
+#
+#     @app.route('/')
+#     def index():
+#         return render_template('index.html')
+#     return app
+#
+# def register_extensions(app):
+#     cors.init_app(app)
+#     db.init_app(app)
+#     api.init_app(app)
+#     # Регистрация 
+#     api.add_namespace(genres_ns)
+#     api.add_namespace(directors_ns)
+#     api.add_namespace(users_ns)
+#     api.add_namespace(movies_ns)
+#     api.add_namespace(auth_ns)
+#
+#     return app
+#
